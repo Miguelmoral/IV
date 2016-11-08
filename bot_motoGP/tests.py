ad6e1a34e2cd8756301d6f80164721e64f0bdae7
Miguel Moral <miguelmoralllamas@correo.ugr.es>
2016-11-09 00:50:25 +0100

    Arreglando errores test.py

--- bot_motoGP/tests.py
+++ bot_motoGP/tests.py
@@ -18,7 +18,7 @@ sesion = unicode('Q2', 'utf-8')
 class Test(unittest.TestCase):
     def test_motogp(self):
         #con = psycopg2.connect(database='motogpbot',user='miguel',password='miguel',host='localhost')
-        con_bd = psycopg2.connect(
+        con = psycopg2.connect(
             database=url.path[1:],
             user=url.username,
             password=url.password,
