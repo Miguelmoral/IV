af0592dd9d8a9248feb9733b8cd0f1852d71cbb3
Miguel Moral <miguelmoralllamas@correo.ugr.es>
2017-01-28 23:36:27 +0100

    Solucionando errores despliege.sh

--- despliege.sh
+++ despliege.sh
@@ -3,17 +3,17 @@
 sudo apt-get update
 
 # Instalación de vagrant
-#wget https://releases.hashicorp.com/vagrant/1.8.7/
-#sudo dpkg -i vagrant_1.8.7_x86_64.deb
+wget https://releases.hashicorp.com/vagrant/1.8.7/
+sudo dpkg -i vagrant_1.8.7_x86_64.deb
 # Instalar plugin para azure
-#vagrant plugin install vagrant-azure
+vagrant plugin install vagrant-azure
 
 # Instalación Ansible
 sudo apt-get install ansible
 
 
 # Despliegue en Azure
-vagrant up --provider=azure
+sudo vagrant up --provider=azure
 
 # Despliegue de la aplicación con Fabric
 sudo pip install fabric
